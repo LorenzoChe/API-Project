769bdd42eb8f57167721f8fda2befefbc5ebdce4
Lorenzo <151394371+LorenzoChe@users.noreply.github.com>
2025-11-18 13:02:55 -0300

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,16 @@
 const header = document.getElementById('header');
 if (header) {
-  ["index.html|Inicio", 
-   "AdivinaElPais.html|Adivina el País", 
-   "ComparacionDosPaises.html|Comparar Países", 
-   "PaisONo.html|¿País o No?", 
+  ["index.html|Inicio",
+   "AdivinaElPais.html|Adivina el País",
+   "ComparacionDosPaises.html|Comparar Países",
+   "PaisONo.html|¿País o No?",
    "PaisVsPais.html|País VS País"]
   .forEach(item => {
     const [href, texto] = item.split("|");
     const a = document.createElement('a');
     a.href = href;
     a.textContent = texto;
+    a.classList.add("nav-link", "px-3", "py-2"); 
     header.appendChild(a);
   });
 }
-
